c10d374b63b1c04649da09993e8e5f21e9c8e1f7
Raspberry Pi <you@example.com>
2019-02-02 18:23:03 +0700

    Привел программу к стандарту POSIX

--- main.c
+++ main.c
@@ -1,9 +1,27 @@
-#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
 
-int main(int argc, const char argv[]) {
-
-    printf("Hello world!\n");
-    return 0;
-
-
+int main()
+{
+    const char * const msg = "Hello World!\n";
+    const char * begin = msg;
+    const char * const end = begin + strlen(msg);
+   
+    while (begin < end)
+    {
+        size_t remaining = end - begin;
+        ssize_t res = write(STDOUT_FILENO, begin, remaining);
+        if (res >= 0)
+        {
+            begin += res;
+            continue; // Let's send the remaining part of this message
+        }
+        if (EINTR == errno)
+        {
+            continue; // It's just a signal, try again
+        }
+        return 1; // It's a real error
+    }
+    return 0; // OK! Let's celebrate and drink some beer!
 }
